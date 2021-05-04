54264667296c99afb88f1cdef5ed80a49a176f1c
Thomas13702 <thomaspritchard137@btinternet.com>
2021-05-04 22:37:42 +0100

    Initial Commit - Pages and routing

---
+++ pages/events/[slug].js
@@ -0,0 +1,11 @@
+//to get dynamic page url put [] around name
+//example: www.mywebsite/events/event_1
+//file would be [event_id(slug)] inside events folder
+
+export default function EventPage() {
+  return (
+    <div>
+      <h1>My Event</h1>
+    </div>
+  );
+}
---
+++ pages/events/index.js
@@ -0,0 +1,9 @@
+//route to index.js inside a folder is same as folder name so here it is events as we are in events folder
+
+export default function EventsPage() {
+  return (
+    <div>
+      <h1>My Events</h1>
+    </div>
+  );
+}
