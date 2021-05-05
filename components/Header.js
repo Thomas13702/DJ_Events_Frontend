d6061243fe90ede3d9f4a27570a55f23d092e15a
Thomas13702 <thomaspritchard137@btinternet.com>
2021-05-05 20:56:32 +0100

    Added Header and Footer

---
+++ components/Header.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import styles from "../styles/Header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <Link href="/">
+          <a>DJ Events</a>
+          {/* if want to style Link tag need to put styles on a tag inside */}
+        </Link>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/events">
+              <a>Events</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
