ed44ac11a8383d376c32d6de4a2f01a5ab9760cd
Thomas13702 <thomaspritchard137@btinternet.com>
2021-05-06 16:35:41 +0100

    Display Events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,30 @@
-//route to index.js inside a folder is same as folder name so here it is events as we are in events folder
-
 import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import { API_URL } from "@/config/index";
 
-export default function EventsPage() {
+export default function EventsPage({ events }) {
+  //catch events as a prop
   return (
     <Layout>
-      <h1>My Events</h1>
+      <h1>Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  //runs on build
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events }, //return events as a prop - > passes events to our client side component as a prop
+    revalidate: 1,
+    //have to have this incase someone updates their data,
+    //as request has already happened on build and update wont be shown, this gets data again after 1 sec if data has change
+  };
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
 import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import Link from "next/link";
 import { API_URL } from "@/config/index";
 
-export default function Home({ events }) {
+export default function HomePage({ events }) {
   //catch events as a prop
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
     </Layout>
   );
 }
@@ -16,7 +29,8 @@ export async function getStaticProps() {
   const events = await res.json();
 
   return {
-    props: { events }, //return events as a prop - > passes events to our client side component as a prop
+    props: { events: events.slice(0, 3) }, //return events as a prop - > passes events to our client side component as a prop
+    //first 3 events
     revalidate: 1,
     //have to have this incase someone updates their data,
     //as request has already happened on build and update wont be shown, this gets data again after 1 sec if data has change
