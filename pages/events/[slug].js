6744b76fa72ca229ae2dcca682b8e2fc6eb35483
Thomas13702 <thomaspritchard137@btinternet.com>
2021-05-06 16:52:49 +0100

    getServerSideProps

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -3,11 +3,23 @@
 //file would be [event_id(slug)] inside events folder
 
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function EventPage() {
+export default function EventPage({ evt }) {
   return (
     <Layout>
-      <h1>My Event</h1>
+      <h1>{evt.name}</h1>
     </Layout>
   );
+}
+
+export async function getServerSideProps({ query: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json(); //return array of one event
+
+  return {
+    props: {
+      evt: events[0],
+    },
+  };
 }
