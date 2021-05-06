500c997a2de90ff0e6db63013f77199f88b39c7b
Thomas13702 <thomaspritchard137@btinternet.com>
2021-05-06 15:52:38 +0100

    getStaticProps()

--- pages/index.js
+++ pages/index.js
@@ -10,13 +10,15 @@ export default function Home({ events }) {
   );
 }
 
-export async function getServerSideProps() {
-  //run on every page load
-  //runs on server first
+export async function getStaticProps() {
+  //runs on build
   const res = await fetch(`${API_URL}/api/events`);
   const events = await res.json();
 
   return {
     props: { events }, //return events as a prop - > passes events to our client side component as a prop
+    revalidate: 1,
+    //have to have this incase someone updates their data,
+    //as request has already happened on build and update wont be shown, this gets data again after 1 sec if data has change
   };
 }
