2fb5819209ede614f312f088ad18a038b4ee4094
Thomas13702 <thomaspritchard137@btinternet.com>
2021-05-06 15:48:27 +0100

    getServerSideProps()

---
+++ config/index.js
@@ -0,0 +1,3 @@
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "httpp://localhost:3000";
+//checks to see if its hosted on another website or localhost:3000
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function Home() {
+export default function Home({ events }) {
+  //catch events as a prop
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  //run on every page load
+  //runs on server first
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events }, //return events as a prop - > passes events to our client side component as a prop
+  };
 }
