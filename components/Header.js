2018fea72727cd6331614ec7f16a006615408410
Thomas13702 <thomaspritchard137@btinternet.com>
2021-05-20 15:05:33 +0100

    Added an Auth Context

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,12 @@
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+import { useContext } from "react"; //have to bring in when you want to use a specific context
 import Link from "next/link";
 import Search from "./Search";
+import AuthContext from "@/context/AuthContext";
 import styles from "@/styles/Header.module.css";
 
 export default function Header() {
+  const { user, logout } = useContext(AuthContext);
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -21,18 +24,40 @@ export default function Header() {
               <a>Events</a>
             </Link>
           </li>
-          <li>
-            <Link href="/events/add">
-              <a>Add Event</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/account/login">
-              <a className="btn-secondary btn-icon">
-                <FaSignInAlt /> Login
-              </a>
-            </Link>
-          </li>
+          {user ? (
+            //if logged in
+            <>
+              <li>
+                <Link href="/events/add">
+                  <a>Add Event</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/account/dashboard">
+                  <a>DashBoard</a>
+                </Link>
+              </li>
+              <li>
+                <button
+                  onClick={() => logout()}
+                  className="btn-secondary btn-icon"
+                >
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            //if not logged in
+            <>
+              <li>
+                <Link href="/account/login">
+                  <a className="btn-secondary btn-icon">
+                    <FaSignInAlt /> Login
+                  </a>
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
